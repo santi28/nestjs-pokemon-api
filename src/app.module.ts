f0dd9cb86f1933de37e4fd7500f7088332e1f860
SantiDN <santidenicolas@gmail.com>
2022-12-13 04:37:16 -0300

    commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { PokemonModule } from './pokemon/pokemon.module';
+
+const MONGO_URI = 'mongodb://root:toor@179.43.127.117:27017/';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    // MongooseModule.forRoot(MONGO_URI, { dbName: 'pokemon' }),
+    PokemonModule,
+  ],
+})
+export class AppModule {}
